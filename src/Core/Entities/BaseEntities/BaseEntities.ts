ca3e2a204c171671c843bee0225d42197c2e60fe
Zenx007 <zenxpokas@gmail.com>
2025-07-17 23:59:22 -0300

    Feat BaseEntities

---
+++ src/Core/Entities/BaseEntities/BaseEntities.ts
@@ -0,0 +1,14 @@
+export class BaseEntities {
+    @CreateDateColumn({name:"created_at", nullable: false})
+    createdAt: Date;
+    
+    @CreateDateColumn({name:"updated_at", nullable: false})
+    updatedAt: Date;
+
+    @CreateDateColumn({nullable:true,name:"disabled_at"})
+    disabledAt?: Date;
+
+    Disable(){
+        this.disabledAt = new Date();
+    }
+}
