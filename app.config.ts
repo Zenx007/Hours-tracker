8fefabf3659eafd18049a80c1a73d5f9992c4972
Zenx007 <zenxpokas@gmail.com>
2025-07-26 18:28:20 -0300

    chore gitignore definitive

--- app.config.ts
+++
@@ -1,8 +0,0 @@
-export const CONFIGURATION = {
-    USERNAME: "postgres",
-    PASSWORD: "postgres123",
-    HOST: "200.196.41.17",
-    PORT: 5432,
-    DATABASE: "HoursTracker",
-    APIBASEURL:"http://localhost:3001"
-}
