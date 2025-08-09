1955c7cf7fc67d1472e53c346655a727e640dff6
Zenx007 <zenxpokas@gmail.com>
2025-08-08 23:45:04 -0300

    feat: refactor and add new interfaces, decorators, and utility functions

--- src/Core/RepositoriesInterfaces/IHoursRecordRepository.interface.ts
+++ src/Core/RepositoriesInterfaces/IHoursRecordRepository.interface.ts
@@ -1,6 +1,11 @@
-import { Task } from "src/Helpers copy/CustomObjects/Task.Interface";
+import { Task } from "src/Helpers/CustomObjects/Task.Interface";
 import { HoursRecord } from "../Entities/HoursRecord/HoursRecord.entity";
+import { Result } from "src/Helpers/CustomObjects/Result";
 
 export abstract class IHoursRecordRepository {
-    abstract Insert (model: HoursRecord) : Task<number>;
+    abstract Insert (model: HoursRecord): Task<number>;
+    abstract Update (model: HoursRecord): Task<number>;
+    abstract Delete (id: number): Task<Result>
+    abstract GetById (id: number): Task<HoursRecord>;
+    abstract GetAll (): Task<HoursRecord[]>;
 }
