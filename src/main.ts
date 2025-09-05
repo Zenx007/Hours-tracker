03ec540d0cb093a7290ee48dd12e5bccec2e66dc
Zenx007 <zenxpokas@gmail.com>
2025-09-04 23:23:02 -0300

    Fix swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
 import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 10000;
+
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3000/",
+    ],
+    methods: "*",
+    credentials: false, 
+  });
+
+ app.use((req: any, res: any, next: () => void) => {
+    next();
+  });
+
+  const staticPath = join(__dirname, "API", "Directory");
+  app.use("/static", express.static(staticPath));
+
+  app.use(bodyParser.json({ limit: '2gb' })); // JSON
+  app.use(bodyParser.urlencoded({ limit: '2gb', extended: true })); // URL encoded
 
   const config = new DocumentBuilder()
     .setTitle('Hours Tracker')
@@ -20,9 +42,17 @@ async function bootstrap() {
     },"authorization") 
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
 
-    SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    swaggerOptions: {
+      tagsSorter: "alpha"
+    }
+  });
+
+  await app.listen(port);
+
+  console.log(`Seja Bem Vindo: Acesse http://localhost:${port}/swagger`)
 
   }
 
